09a27ce09fa2a89c0d07df707f2186ab5d5d47ac
VilladaEdwinMSTR <93624470+VilladaEdwinMSTR@users.noreply.github.com>
2023-04-24 07:20:09 -0700

    24/4/2023

--- Restaurante - reporte/PL/frmIngredientes.cs
+++ Restaurante - reporte/PL/frmIngredientes.cs
@@ -21,6 +21,62 @@ namespace Restaurante___reporte.PL
         {
 
         }
+        //VALIDACION DE VALORES INGRESADOS POR EL USUARIO
+        public bool ValoresVaciosIngredientes()
+        {
+            //string valoresVacios = "";
+            //int no_vacios = 0;
+            ////VERIFICACION DE VALORES VACIOS
+            //{
+            //    if (txtIngredienteId.Text == "")
+            //    {
+            //        valoresVacios += "id_platillo, ";
+            //        no_vacios++;
+            //    }
+            //    if (txtNombre.Text == "")
+            //    {
+            //        valoresVacios += "Nombre, ";
+            //        no_vacios++;
+            //    }
+            //    if (cbCategoria.Text == "-- Categoria --")
+            //    {
+            //        valoresVacios += "Categoria, ";
+            //        no_vacios++;
+            //    }
+            //    if (cbDificultad.Text == "-- Dificultad --")
+            //    {
+            //        valoresVacios += "Dificultad, ";
+            //        no_vacios++;
+            //    }
+            //    if (txtPrecio.Text == "")
+            //    {
+            //        valoresVacios += "Precio, ";
+            //        no_vacios++;
+            //    }
+            //    if (txtDescripcion.Text == "")
+            //    {
+            //        valoresVacios += "Descripcion,";
+            //        no_vacios++;
+            //    }
+            //    if (pbFotoPlatillo.Image == Properties.Resources.estilo_grafico__1_)
+            //    {
+            //        valoresVacios += "Imagen";
+            //        no_vacios++;
+            //    }
+
+            //}
+            //if (no_vacios > 0)
+            //{
+            //    MessageBox.Show("No puede dejar información en blanco \r\n\r\n" +
+            //                    "No. de valores vacios: " + no_vacios + "\r\n" +
+            //                    "Valores vacios: " + valoresVacios, "ERROR AL INGRESAR VALORES");
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+        }
 
         private void txtIngredienteId_TextChanged(object sender, EventArgs e)
         {
